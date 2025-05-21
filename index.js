3cadbab8ea0eedd2cd7ffe05d0ccba8ce655568c
Sdqumar <sdqumar09@gmail.com>
2025-05-21 13:47:22 +0100

    fix: Update Billing Center navigation button with new SVG asset and correct background image

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ newbutton
   .querySelector(".nav-bg-image")
   .setAttribute(
     "style",
-    "background-position: 0;background-image:url('path/to/image.jpg')",
+    "background-position: 0; background-image: url('/asset/billing-center.svg');",
   );
 
 newbutton.querySelector("a").addEventListener("click", function (e) {
