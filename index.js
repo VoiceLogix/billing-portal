3a934e1f23e7d26b758b29da461741f581f925d2
Sdqumar <sdqumar09@gmail.com>
2025-05-21 14:06:21 +0100

    fix: Adjust background image style for Billing Center navigation button

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ newbutton
   .querySelector(".nav-bg-image")
   .setAttribute(
     "style",
-    "background-position: 0; background-image: url('https://raw.githubusercontent.com/VoiceLogix/billing-portal/refs/heads/main/public/asset/billing-center.svg');",
+    "background-position: 10px; background-size: 70%; background-image: url('https://raw.githubusercontent.com/VoiceLogix/billing-portal/refs/heads/main/public/asset/billing-center.svg');",
   );
 
 newbutton.querySelector("a").addEventListener("click", function (e) {
