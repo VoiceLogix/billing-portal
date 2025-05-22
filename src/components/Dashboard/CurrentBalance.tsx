e9a24be1c85e04b71174000a93427fcef83358f8
Sdqumar <sdqumar09@gmail.com>
2025-05-22 08:21:49 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/Dashboard/CurrentBalance.tsx
+++ src/components/Dashboard/CurrentBalance.tsx
@@ -7,43 +7,45 @@ export const CurrentBalance = () => {
   console.log("subscriber", subscriber);
 
   return (
-    <Box className="layoutWithBorder" width="368px">
+    <>
       {subscriber && (
-        <Flex direction="column" gap="5">
-          <Flex direction="column" gap="2">
-            <Text size="2" color="gray">
-              Current Balance
-            </Text>
-            <Heading size="5" weight="medium">
-              {formatToUSD(subscriber.accountBalance.totalAmount)}
-            </Heading>
-          </Flex>
+        <Box className="layoutWithBorder" width="368px">
+          <Flex direction="column" gap="5">
+            <Flex direction="column" gap="2">
+              <Text size="2" color="gray">
+                Current Balance
+              </Text>
+              <Heading size="5" weight="medium">
+                {formatToUSD(subscriber.accountBalance.totalAmount)}
+              </Heading>
+            </Flex>
 
-          <Flex direction="column" gap="3">
-            <Flex gap="4" justify="between" px="2">
-              <Text size="2">Last Invoice</Text>
-              <Text size="2" color="gray">
-                Apr 25, 2025
-              </Text>
-              <Text size="2">$68.00</Text>
-            </Flex>
-            <hr
-              style={{
-                border: "1px solid #E6E6E6",
-              }}
-            />
-            <Flex gap="4" justify="between" px="2">
-              <Text size="2">Last Invoice</Text>
-              <Text size="2" color="gray">
-                Apr 25, 2025
-              </Text>
-              <Text size="2">$68.00</Text>
+            <Flex direction="column" gap="3">
+              <Flex gap="4" justify="between" px="2">
+                <Text size="2">Last Invoice</Text>
+                <Text size="2" color="gray">
+                  Apr 25, 2025
+                </Text>
+                <Text size="2">$68.00</Text>
+              </Flex>
+              <hr
+                style={{
+                  border: "1px solid #E6E6E6",
+                }}
+              />
+              <Flex gap="4" justify="between" px="2">
+                <Text size="2">Last Invoice</Text>
+                <Text size="2" color="gray">
+                  Apr 25, 2025
+                </Text>
+                <Text size="2">$68.00</Text>
+              </Flex>
             </Flex>
           </Flex>
-        </Flex>
+        </Box>
       )}
       {isLoading && <p>Loading...</p>}
       {isError && <p>Error loading subscriber data</p>}
-    </Box>
+    </>
   );
 };
