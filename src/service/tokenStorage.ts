838359ec5719e4efe3aa444a328ef2d6306c6ce8
Sdqumar <sdqumar09@gmail.com>
2025-08-13 12:47:52 +0100

    Update API base URL for billing service and adjust token storage configuration
    - Changed the base URL from localhost to the production billing API endpoint.
    - Commented out the localhost URL for clarity and future reference.

--- src/service/tokenStorage.ts
+++ src/service/tokenStorage.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { BillingAuthResponse } from "../types/BillingSubscriberResult";
 
-// export const baseURL = "https://sb-manage.unitydial.com/billing-api/";
-export const baseURL = "http://localhost:8085/";
+export const baseURL = "https://sb-manage.unitydial.com/billing-api/";
+// export const baseURL = "http://localhost:8085/";
 export const AUTH_URL = `${baseURL}auth`;
 
 export const LS_KEY_USER_AUTH = "billing_user_auth";
